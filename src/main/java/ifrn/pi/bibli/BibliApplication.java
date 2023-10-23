cae3182da5d22bbe07065168c124c8a925b2212f
Karol <elayne.sabino@escolar.ifrn.edu.br>
2023-10-23 14:04:22 -0300

    Colocando os arquivos iniciais da minha aplicação

---
+++ src/main/java/ifrn/pi/bibli/BibliApplication.java
@@ -0,0 +1,13 @@
+package ifrn.pi.bibli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliApplication.class, args);
+	}
+
+}

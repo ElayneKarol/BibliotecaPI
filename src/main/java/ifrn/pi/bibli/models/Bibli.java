e9775ace45adcabe965b9dbabddd27775ca6ead1
Karol <elayne.sabino@escolar.ifrn.edu.br>
2023-10-23 16:20:26 -0300

    Configurando dependências do banco de dados(pom.xml); Configurando bd(src/main/resources/application.properties); Configurando clase de modelo biblioteca e cadastro(modified:   src/main/java/ifrn/pi/bibli/models/Bibli.java         modified:   src/main/java/ifrn/pi/bibli/models/Cadastro.java)

--- src/main/java/ifrn/pi/bibli/models/Bibli.java
+++ src/main/java/ifrn/pi/bibli/models/Bibli.java
@@ -1,12 +1,30 @@
 package ifrn.pi.bibli.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Bibli {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String titulo;
 	private String autor;
 	private String editora;
 	private String dataPubli;
 	private String edicao;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 
 	public String getTitulo() {
 		return titulo;
@@ -40,11 +58,11 @@ public class Bibli {
 		this.dataPubli = dataPubli;
 	}
 
-	public String getedicao() {
+	public String getEdicao() {
 		return edicao;
 	}
 
-	public void setDataEdi(String edicao) {
+	public void setEdicao(String edicao) {
 		this.edicao = edicao;
 	}
 
--- src/main/java/ifrn/pi/bibli/models/Cadastro.java
+++ src/main/java/ifrn/pi/bibli/models/Cadastro.java
@@ -1,12 +1,28 @@
 package ifrn.pi.bibli.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Cadastro {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String nome;
 	private String matricula;
 	private String cpf;
 	private String dataNasc;
 	private String endereco;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	public String getNome() {
 		return nome;
 	}

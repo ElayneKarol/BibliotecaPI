13d41f5ca19068e72b8925d249a33d675b53d13c
Karol <elayne.sabino@escolar.ifrn.edu.br>
2023-10-23 15:24:45 -0300

    Criando modelo para cadastro

---
+++ src/main/java/ifrn/pi/bibli/models/Cadastro.java
@@ -0,0 +1,41 @@
+package ifrn.pi.bibli.models;
+
+public class Cadastro {
+
+	private String nome;
+	private String matricula;
+	private String cpf;
+	private String dataNasc;
+	private String endereco;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getDataNasc() {
+		return dataNasc;
+	}
+	public void setDataNasc(String dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+}
